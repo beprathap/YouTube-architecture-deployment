649a6b1b22809bf3b7a4ea19e3b0fe71afa435ec
Prathap Boddu <beprathap@Mac.attlocal.net>
2024-11-23 00:50:47 -0600

    add sample buckets creation

--- s3_setup.py
+++ s3_setup.py
@@ -1,13 +1,40 @@
 # Deploy S3 buckets
 import boto3
+from botocore.exceptions import ClientError
 
-client = boto3.client('s3', region_name='us-east-1')
+# AWS region
+region = "us-east-1" # Region can be updated as needed
 
-client.create_bucket(Bucket='prathapbucket-farmersbranch')
+# Bucket names
+buckets = [
+    "youtube-raw-data-bucket",
+    "youtube-transformed-data-bucket",
+    "youtube-mwaa-dags-bucket"
+]
 
-with open('myfile.txt', 'w') as file:
-    file.write('This is a text file for upload')
+# Initialize S3 client
+s3_client = boto3.client("s3", region_name=region)
 
-client.upload_file(Filename='myfile.txt',
-                   Bucket='prathapbucket-farmersbranch',
-                   Key='test-upload-file')
+def create_bucket(bucket_name, region):
+    """
+    Creates an S3 bucket in the specified region
+    :param bucket_name:
+    :param region:
+    :return:
+    """
+
+    try:
+        if region == "us-east-1":
+            s3_client.create_bucket(Bucket=bucket_name)
+        else:
+            s3_client.create_bucket(
+                Bucket=bucket_name,
+                CreateBucketConfiguration={"LocationConstraint":region}
+            )
+        print(f"Bucket {bucket_name} created successfully.")
+    except ClientError as e:
+        print(f"Error creating bucket {bucket_name}: {e}")
+
+# Create buckets
+for bucket in buckets:
+    create_bucket(bucket, region)
